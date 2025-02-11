5342b08e0834b292efa65e7b56117adf110feac7
Timofey Zaberejny <jfomit@gmail.com>
2025-02-11 20:58:23 +0300

    Added some code.

---
+++ Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите делимое и делитель:");
+if (!TryReadDouble2(out var dividend, out var divisor))
+{
+    Console.WriteLine("Некорректный ввод.");
+    return 1;
+}
+
+var quotient = dividend / divisor;
+Console.WriteLine($"Частное: {quotient}.");
+return 0;
+
+static bool TryReadDouble2(out double a, out double b)
+{
+    (a, b) = (0, 0);
+    return double.TryParse(Console.ReadLine(), out a)
+           && double.TryParse(Console.ReadLine(), out b);
+}
